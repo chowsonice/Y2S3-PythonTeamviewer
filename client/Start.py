cd9125c4f0902d8495cb8017483bdda7ac70cbc4
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-08-17 20:00:39 +0700

    Mostly functional Application and Take Pic

--- client/Start.py
+++ client/Start.py
@@ -6,28 +6,22 @@ class Start(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Start")
-        self.create_widgets()
 
-    def create_widgets(self):
         self.txtID = tk.Entry(self)
         self.txtID.pack()
 
-        button = tk.Button(self, text="Start", command=self.start_button_click)
-        button.pack()
+        self.butStart = tk.Button(self, text="Start", command=self.start_application)
+        self.butStart.pack()
 
-        self.protocol("WM_DELETE_WINDOW", self.window_closing)
+        self.protocol("WM_DELETE_WINDOW", self.on_close)
 
-    def start_button_click(self):
-        Program.nw.write("STARTID\n")
-        Program.nw.flush()
+    def start_application(self):
         Program.nw.write(self.txtID.get() + "\n")
         Program.nw.flush()
-        s = Program.nr.readline()
-        messagebox.showinfo("Response", s)
+        s = Program.nr.readline().strip()
+        messagebox.showinfo("Message", s)
 
-    def window_closing(self):
+    def on_close(self):
         Program.nw.write("QUIT\n")
         Program.nw.flush()
         self.destroy()
-
-
--- client/listApp.py
+++ client/listApp.py
@@ -1,117 +1,72 @@
 import tkinter as tk
-import socket
 from tkinter import messagebox
+from Program import Program
+from Start import Start
+from Kill import Kill
 
-class ListAppForm(tk.Tk):
-    def __init__(self, server_address, server_port):
+class ListApp(tk.Tk):
+    def __init__(self):
         super().__init__()
+        self.title("List App")
 
-        self.title("List Applications")
-        self.geometry("400x300")
-
-        self.button_kill = tk.Button(self, text="Kill", command=self.open_kill_form)
+        self.button_kill = tk.Button(self, text="KILL", command=self.kill_application)
         self.button_kill.pack()
 
-        self.button_view = tk.Button(self, text="View", command=self.send_view)
+        self.button_view = tk.Button(self, text="VIEW", command=self.view_applications)
         self.button_view.pack()
 
-        self.button_start = tk.Button(self, text="Start", command=self.open_start_form)
+        self.button_start = tk.Button(self, text="START", command=self.start_application)
         self.button_start.pack()
 
-        self.button_clear = tk.Button(self, text="Clear", command=self.clear_list)
+        self.button_clear = tk.Button(self, text="CLEAR", command=self.clear_list)
         self.button_clear.pack()
 
         self.list_view = tk.Listbox(self)
         self.list_view.pack()
 
-        self.protocol("WM_DELETE_WINDOW", self.send_quit)
+        self.protocol("WM_DELETE_WINDOW", self.on_close)
 
-        self.server_address = server_address
-        self.server_port = server_port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.server_address, self.server_port))
+    def kill_application(self):
+        temp = "KILL"
+        Program.nw.write(temp + "\n")
+        Program.nw.flush()
+        view_kill = Kill()
+        view_kill.mainloop()
 
-    def send_message(self, message):
-        self.socket.sendall(message.encode())
+    def view_applications(self):
+        temp = "XEM"
+        Program.nw.write(temp + "\n")
+        Program.nw.flush()
+        s1 = "Name application"
+        s2 = "ID"
+        s3 = "Count"
+        temp = Program.nr.readline().strip()
+        print(temp)
+        soprocess = int(temp)
+        for _ in range(soprocess):
+            s1 = Program.nr.readline().strip()
+            s2 = Program.nr.readline().strip()
+            s3 = Program.nr.readline().strip()
+            one = [s1, s2, s3]
+            print(one)
+            self.list_view.insert(tk.END, one)
 
-    def receive_message(self):
-        return self.socket.recv(1024).decode()
+    def start_application(self):
+        temp = "START"
+        Program.nw.write(temp + "\n")
+        Program.nw.flush()
+        view_start = Start()
+        view_start.mainloop()
 
-    def open_kill_form(self):
-        self.send_message("KILL")
-        kill_form = KillForm(self)
-        kill_form.mainloop()
-
-    def send_view(self):
-        self.send_message("XEM")
-        response = self.receive_message()
-        process_count = int(response)
-
-        self.list_view.delete(0, tk.END)
-        for _ in range(process_count):
-            response = self.receive_message()
-            if response == "ok":
-                name = self.receive_message()
-                process_id = self.receive_message()
-                count = self.receive_message()
-                item = f"Name: {name}, ID: {process_id}, Count: {count}"
-                self.list_view.insert(tk.END, item)
-
-    def open_start_form(self):
-        self.send_message("START")
-        start_form = StartForm(self)
-        start_form.mainloop()
-
-    def send_quit(self):
-        self.send_message("QUIT")
-        self.socket.close()
+    def on_close(self):
+        s = "QUIT"
+        Program.nw.write(s + "\n")
+        Program.nw.flush()
         self.destroy()
 
     def clear_list(self):
-        self.list_view.delete(0, tk.END)
+        list_view.delete(0, tk.END)
 
-class KillForm(tk.Toplevel):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.title("Kill Process")
-        self.geometry("300x200")
-
-        self.label_id = tk.Label(self, text="Enter Process ID:")
-        self.label_id.pack()
-
-        self.entry_id = tk.Entry(self)
-        self.entry_id.pack()
-
-        self.button_kill = tk.Button(self, text="Kill", command=self.send_kill)
-        self.button_kill.pack()
-
-    def send_kill(self):
-        process_id = self.entry_id.get()
-        self.master.send_message("KILLID")
-        self.master.send_message(process_id)
-        response = self.master.receive_message()
-        messagebox.showinfo("Result", response)
-
-class StartForm(tk.Toplevel):
-    def __init__(self, master):
-        super().__init__(master)
-
-        self.title("Start Process")
-        self.geometry("300x200")
-
-        self.label_name = tk.Label(self, text="Enter Process Name:")
-        self.label_name.pack()
-
-        self.entry_name = tk.Entry(self)
-        self.entry_name.pack()
-
-        self.button_start = tk.Button(self, text="Start", command=self.send_start)
-        self.button_start.pack()
-
-    def send_start(self):
-        process_name = self.entry_name.get()
-        self.master.send_message("STARTAPP")
-        self.master.send_message(process_name)
-        response = self.master.receive_message()
-        messagebox.showinfo("Result", response)
+if __name__ == '__main__':
+    app = ListApp()
+    app.mainloop()
